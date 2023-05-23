bd9a7c85a22809fe7041d782134ba03ebebadeb7
bloom-tools <134373822+bloom-tools@users.noreply.github.com>
2023-05-23 17:54:50 +0200

    Added mushroom beat time calc.

--- dist/bloom-tools.js
+++ dist/bloom-tools.js
@@ -1,19 +1,9 @@
 $(document).ready(function()
 {
-	// MUSHROOMS
-	// let mushroomSize = ['Small', 'Normal', 'Large'];
-	// let mushroomBrown = [51800, 397400, 1728000]; //Likely extinct, R.I.P
-
-	// let mushroomRed = [87400, 670600, 2916000];
-	// let mushroomYellow = [84200, 645800, 2808000];
-	// let mushroomBlue = [84200, 645800, 2808000];
-	// let mushroomPurple = [93900, 720300, 3132000];
-	// let mushroomWhite = [81000, 621000, 2700000];
-	// let mushroomPink = [81000, 621000, 2700000];
-	// let mushroomGray = [90700, 695500, 3024000];
-	// let mushroomFire = [0, 3850200, 13662000];
-	// let mushroomWater = [0, 3816700, 13543200];
-	// let mushroomCrystal = [0, 3883600, 13780800];
+	/*
+	Pikmin order is: Red, Yellow, Blue, Purple, White, Pink, Gray.
+	Mushroom order is same as pikmin order, followed by Fire, Water and Crystal.
+	*/
 
 	let mushroomHealths = [
 		[87400, 670600, 2916000], //Red
@@ -30,16 +20,10 @@ $(document).ready(function()
 
 	// PIKMINS
 	let attackPower = [4, 3, 3, 6, 2, 2, 5];
-	//let bloomBonus = [0, 1, 2, 3, 4]; //Index == value!
-
+	//let bloomBonus = [0, 1, 2, 3, 4]; //Not needed since index == value!
 	let decorBonus = 4;
 	let friendshipBonuses = [0, 1, 2, 3, 4, 8, 12, 16, 20];
 
-	/*
-		Values taken from the Attack Power Infographic v2
-		Pikmin order is: Red, Yellow, Blue, Purple, White, Pink, Gray.
-		Mushroom order is same as pikmin order, followed by Fire, Water and Crystal.
-	*/
 	let mushroomMatchup = [
 		[12, 3, 0, 3, 0, 3, 0, 100, 0, 0],
 		[3, 12, 0, 3, 0, 3, 3, 0, 0, 0],
@@ -50,10 +34,24 @@ $(document).ready(function()
 		[3, 3, 0, 3, 0, 3, 12, 0, 0, 100],
 	];
 
+	function secondsToHMS(s)
+	{
+		let h = Math.floor(s / 3600);
+		s -= h * 3600;
+
+		let m = Math.floor(s / 60);
+		s -= m * 60;
+
+		s = Math.trunc(s);
+
+		return h + ' hour(s), ' + m + ' minute(s) and ' + s + ' second(s)';
+	}
+
 	function getStrength()
 	{
 		let individualStrength = squadStrength = 0;
-		
+		let smallTime = normalTime = largeTime = 0;
+
 		let pikminType = $('#pikminType')[0].selectedIndex;
 		let bloomBonus = $('#bloomBonus')[0].selectedIndex;
 		let mushroomType = $('#mushroomType')[0].selectedIndex;
@@ -98,6 +96,15 @@ $(document).ready(function()
 		squadStrength = individualStrength * squadSize;
 		$('.individual').html(individualStrength);
 		$('.squad').html(squadStrength);
+
+		//Calculate Mushroom Time
+		smallTime = (mushroomHealths[mushroomType][0] / squadStrength) * 100;
+		normalTime = (mushroomHealths[mushroomType][1] / squadStrength) * 100;
+		largeTime = (mushroomHealths[mushroomType][2] / squadStrength) * 100;
+
+		$('.smallTime').html(secondsToHMS(smallTime));
+		$('.normalTime').html(secondsToHMS(normalTime));
+		$('.largeTime').html(secondsToHMS(largeTime));
 	}
 
 
